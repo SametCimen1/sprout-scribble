7b44975ac812e79bdc0cdc29d2f89795915a16f6
Samet Cimen <cimensamet338@gmail.com>
2024-06-07 16:31:26 -0400

    define config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,16 @@
-
-import type { Config } from 'drizzle-kit';
-
-import * as dotenv from 'dotenv';
+import { defineConfig } from "drizzle-kit"
+import * as dotenv from "dotenv"
 
 dotenv.config({
-    path: ".env.local"
+  path: ".env.local",
 })
 
-export default({
-  schema: './server/schema.ts',
-  out: './server/migrations',
-  dialect: 'postgresql', // 'postgresql' | 'mysql' | 'sqlite'
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./server/schema.ts",
+  out: "./server/migrations",
   dbCredentials: {
-    url: process.env.POSTGRES_URL!
+    url: process.env.POSTGRES_URL!,
   },
-});
+})
